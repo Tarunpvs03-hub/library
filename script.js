8857a02522482c672acd98b1cb7a7cfd61cf1406
TarunPVS <tarunpekala03@gmail.com>
2026-06-08 21:11:15 +0530

    completed design and basic logic

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const readCheckbox = document.querySelector("#read");
-function book(title, author, pages, read) {
+const container = document.querySelector(".library-container");
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -23,6 +24,45 @@ form.addEventListener("submit", (e) => {
   );
 
   myLibrary.push(newBook);
-
+  displayLibrary();
+  form.reset();
   console.log(myLibrary);
 });
+
+function displayLibrary() {
+  container.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    const card = `<div class="card">
+    <h2>${book.title}</h2>  
+    <p>Author: ${book.author}</p>
+    <p>Pages: ${book.pages}</p>
+    <p>Read: ${book.read ? "Yes" : "No"}</p>
+  </div>
+
+  
+     <button class="toggle-btn" data-index="${index}">
+      Toggle Read
+    </button>
+  <button class="delete-btn" data-index="${index}">Delete</button>`;
+    container.innerHTML += card;
+  });
+
+  const toggleButtons = document.querySelectorAll(".toggle-btn");
+
+  toggleButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const index = Number(e.target.dataset.index);
+      myLibrary[index].read = !myLibrary[index].read;
+      displayLibrary();
+    });
+  });
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const index = Number(e.target.dataset.index);
+      myLibrary.splice(index, 1);
+      displayLibrary();
+    });
+  });
+}
